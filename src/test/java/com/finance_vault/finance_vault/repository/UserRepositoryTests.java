bcd94556e4a88b056c1ef57f676b11032d27bb76
Nicolas Paiva <nicolaspaiva@outlook.com>
2025-06-16 21:23:58 +0100

    Developed more tests for the UserRepository and TransactionService

--- src/test/java/com/finance_vault/finance_vault/repository/UserRepositoryTests.java
+++ src/test/java/com/finance_vault/finance_vault/repository/UserRepositoryTests.java
@@ -3,13 +3,16 @@ package com.finance_vault.finance_vault.repository;
 import com.finance_vault.finance_vault.model.user.User;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
-import org.junit.runner.RunWith; // ⬅️ Add this
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner; // ⬅️ And this
+import org.springframework.test.context.junit4.SpringRunner;
+
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
@@ -19,10 +22,12 @@ public class UserRepositoryTests {
     @Autowired
     private UserRepository userRepository;
 
-    @Test
-    public void UserRepository_SaveAll_ReturnSavedUser() {
+
+    @Test(expected = RuntimeException.class)
+    public void UserRepository_ThrowException_WhenIdIsProvided() {
         // Arrange
         User user = User.builder()
+                .id(1L)
                 .name("Nicolas")
                 .lastName("Paiva")
                 .email("abc@abc.com")
@@ -38,4 +43,90 @@ public class UserRepositoryTests {
         Assertions.assertThat(savedUser).isNotNull();
         Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
     }
+
+    @Test
+    public void UserRepository_ShouldThrowException_WhenEmailIsMissing() {
+        // Arrange
+        User user = User.builder()
+                .name("Nicolas")
+                .lastName("Paiva")
+                .password("abc")
+                .balance(1999)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        // Act & Assert
+        Assertions.assertThatThrownBy(() -> {
+            userRepository.save(user);
+            userRepository.flush(); // Forces DB insert
+        }).isInstanceOf(DataIntegrityViolationException.class);
+    }
+
+
+    @Test
+    public void UserRepository_GetAll_ReturnMoreThanOneUser() {
+        // Arrange
+        User user1 = User.builder()
+                .name("Nicolas")
+                .lastName("Paiva")
+                .email("abc@abc.com")
+                .password("abc")
+                .balance(1999)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        User user2 = User.builder()
+                .name("Beatriz")
+                .lastName("Baia")
+                .email("abc@def.com")
+                .password("abc")
+                .balance(3500)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        // Act
+        userRepository.save(user1);
+        userRepository.save(user2);
+
+        List<User> users = userRepository.findAll();
+
+        // Assert
+        Assertions.assertThat(users.size()).isEqualTo(2);
+
+    }
+
+
+    @Test(expected = DataIntegrityViolationException.class)
+    public void ShouldThrowException_WhenEmailIsDuplicated() {
+        // Arrange
+        User user1 = User.builder()
+                .name("Nicolas")
+                .lastName("Paiva")
+                .email("abc@abc.com")
+                .password("abc")
+                .balance(1999)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        User user2 = User.builder()
+                .name("Beatriz")
+                .lastName("Baia")
+                .email("abc@abc.com")
+                .password("abc")
+                .balance(3500)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        // Act
+        userRepository.save(user1);
+        userRepository.save(user2);
+
+        List<User> users = userRepository.findAll();
+
+        // Assert
+        Assertions.assertThat(users).isNotNull();
+        Assertions.assertThat(users.size()).isEqualTo(2);
+
+    }
+
 }
---
+++ src/test/java/com/finance_vault/finance_vault/service/transaction/TransactionServiceTests.java
@@ -0,0 +1,196 @@
+package com.finance_vault.finance_vault.service.transaction;
+
+import com.finance_vault.finance_vault.dto.transaction.TransactionRequest;
+import com.finance_vault.finance_vault.exception.InvalidTransactionException;
+import com.finance_vault.finance_vault.model.transaction.Transaction;
+import com.finance_vault.finance_vault.model.user.User;
+import com.finance_vault.finance_vault.repository.TransactionRepository;
+import com.finance_vault.finance_vault.service.user.UserService;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@ExtendWith(MockitoExtension.class)
+public class TransactionServiceTests {
+
+    @Mock
+    private TransactionRepository transactionRepository;
+
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private TransactionServiceImpl transactionService;
+
+
+    @Test
+    public void TransactionService_CreateTransaction() {
+        // Arrange
+        User sender = User.builder()
+                .id(1L)
+                .name("A")
+                .lastName("B")
+                .email("abc@abc.com")
+                .balance(1000)
+                .sentTransactions(new ArrayList<>())
+                .receivedTransactions(new ArrayList<>())
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        User receiver = User.builder()
+                .id(2L)
+                .name("D")
+                .lastName("E")
+                .email("def@def.com")
+                .balance(1000)
+                .sentTransactions(new ArrayList<>())
+                .receivedTransactions(new ArrayList<>())
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        TransactionRequest transaction = new TransactionRequest();
+        transaction.setAmount(100);
+        transaction.setReceiverEmail("def@def.com");
+
+        // Mocks the methods within the TransactionService
+        when(userService.getUserFromEmail(sender.getEmail())).thenReturn(Optional.of(sender));
+        when(userService.getUserFromEmail(receiver.getEmail())).thenReturn(Optional.of(receiver));
+        when(transactionRepository.save(Mockito.any(Transaction.class))).thenReturn(null);
+
+        transactionService.createTransaction(transaction, sender);
+
+        // Assert
+        Assertions.assertThat(sender.getBalance()).isEqualTo(900);
+        Assertions.assertThat(receiver.getBalance()).isEqualTo(1100);
+    }
+
+
+    @Test(expected = InvalidTransactionException.class)
+    public void TransactionService_ThrowException_WhenSelfTransaction() {
+        // Arrange
+        User sender = User.builder()
+                .id(1L)
+                .name("A")
+                .lastName("B")
+                .email("abc@abc.com")
+                .balance(1000)
+                .sentTransactions(new ArrayList<>())
+                .receivedTransactions(new ArrayList<>())
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        User receiver = User.builder()
+                .id(2L)
+                .name("D")
+                .lastName("E")
+                .email("abc@abc.com")
+                .balance(1000)
+                .sentTransactions(new ArrayList<>())
+                .receivedTransactions(new ArrayList<>())
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        TransactionRequest transaction = new TransactionRequest();
+        transaction.setAmount(100);
+        transaction.setReceiverEmail("def@def.com");
+
+        // ACT
+        // Mocks the methods within the TransactionService
+        when(userService.getUserFromEmail(sender.getEmail())).thenReturn(Optional.of(sender));
+        when(userService.getUserFromEmail(receiver.getEmail())).thenReturn(Optional.of(receiver));
+        when(transactionRepository.save(Mockito.any(Transaction.class))).thenReturn(null);
+
+        transactionService.createTransaction(transaction, sender);
+    }
+
+
+    @Test(expected = InvalidTransactionException.class)
+    public void TransactionService_ThrowException_WhenBalanceIsNotEnough() {
+        // Arrange
+        User sender = User.builder()
+                .id(1L)
+                .name("A")
+                .lastName("B")
+                .email("abc@abc.com")
+                .balance(1000)
+                .sentTransactions(new ArrayList<>())
+                .receivedTransactions(new ArrayList<>())
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        User receiver = User.builder()
+                .id(2L)
+                .name("D")
+                .lastName("E")
+                .email("def@def.com")
+                .balance(1000)
+                .sentTransactions(new ArrayList<>())
+                .receivedTransactions(new ArrayList<>())
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        TransactionRequest transaction = new TransactionRequest();
+        transaction.setAmount(5000);
+        transaction.setReceiverEmail("def@def.com");
+
+        // ACT
+        // Mocks the methods within the TransactionService
+        when(userService.getUserFromEmail(sender.getEmail())).thenReturn(Optional.of(sender));
+        when(userService.getUserFromEmail(receiver.getEmail())).thenReturn(Optional.of(receiver));
+        when(transactionRepository.save(Mockito.any(Transaction.class))).thenReturn(null);
+
+        transactionService.createTransaction(transaction, sender);
+    }
+
+
+    @Test(expected = InvalidTransactionException.class)
+    public void TransactionService_ThrowException_WhenTransactionAmountIsNegative() {
+        // Arrange
+        User sender = User.builder()
+                .id(1L)
+                .name("A")
+                .lastName("B")
+                .email("abc@abc.com")
+                .balance(1000)
+                .sentTransactions(new ArrayList<>())
+                .receivedTransactions(new ArrayList<>())
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        User receiver = User.builder()
+                .id(2L)
+                .name("D")
+                .lastName("E")
+                .email("def@def.com")
+                .balance(1000)
+                .sentTransactions(new ArrayList<>())
+                .receivedTransactions(new ArrayList<>())
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        TransactionRequest transaction = new TransactionRequest();
+        transaction.setAmount(-400);
+        transaction.setReceiverEmail("def@def.com");
+
+        // ACT
+        // Mocks the methods within the TransactionService
+        when(userService.getUserFromEmail(sender.getEmail())).thenReturn(Optional.of(sender));
+        when(userService.getUserFromEmail(receiver.getEmail())).thenReturn(Optional.of(receiver));
+        when(transactionRepository.save(Mockito.any(Transaction.class))).thenReturn(null);
+
+        transactionService.createTransaction(transaction, sender);
+    }
+}
